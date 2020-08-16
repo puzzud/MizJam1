8b01c9bb41817ce01e5bf1c69f986906b293b1c5
puzzud <puzzud@gmail.com>
2020-08-16 19:53:29 -0400

    Added acceleration, friction, and drag.

--- Project/Scripts/Kart.gd
+++ Project/Scripts/Kart.gd
@@ -1,9 +1,18 @@
 extends KinematicBody
 class_name Kart
 
-export (float) var topSpeed := 10.0
+const nominalVelocityLength := 0.4
+const wheelBase := 0.5 # TODO: Needs to be based on scene.
+const steeringAngle := 1.0
 
-var velocity: Vector3 = Vector3.ZERO
+export (float) var enginePower := 25.0
+export (float) var friction := -1.9
+export (float) var drag := -0.00001
+
+var acceleration := Vector3.ZERO
+var velocity := Vector3.ZERO
+var steerDirection := 0.0
+
 var intendedTurnDirection := 0.0
 var intendedAccelerating := false
 
@@ -18,22 +27,50 @@ func _process(delta: float) -> void:
 	if controller != null:
 		intendedTurnDirection = controller.turnDirection
 		intendedAccelerating = controller.accelerating
+	
+	steerDirection = deg2rad(-intendedTurnDirection * steeringAngle)
 
 func _physics_process(delta: float) -> void:
+	acceleration = Vector3.ZERO
+	
 	if intendedAccelerating:
-		var direction: Vector3 = -transform.basis.z
-		
-		if intendedTurnDirection != 0.0:
-			#direction = direction.rotated(Vector3.UP, deg2rad(-intendedTurnDirection))
-			rotate(Vector3.UP, deg2rad(-intendedTurnDirection))
-		
-		velocity = direction * topSpeed
-		velocity.y = 0.0
-		
-		velocity = move_and_slide(velocity, Vector3(0.0, -1.0, 0.0))
+		acceleration = -transform.basis.z * enginePower
+	
+	applyFriction()
+	
+	calculateSteering(delta)
+	
+	velocity += acceleration * delta
+	velocity.y = 0.0
+	velocity = move_and_slide(velocity, Vector3.UP)
 
 func getController() -> Controller:
 	if has_node("Controller"):
 		return $Controller as Controller
 	
 	return null
+
+func applyFriction() -> void:
+	if velocity.length() < nominalVelocityLength:
+		velocity = Vector3.ZERO
+	
+	var frictionForce := velocity * friction
+	var dragForce = velocity * velocity.length() * drag
+	
+	acceleration += dragForce + frictionForce
+
+func calculateSteering(delta: float) -> void:
+	var rearWheel = transform.origin + transform.basis.z * wheelBase / 2.0
+	var frontWheel = transform.origin - transform.basis.z * wheelBase / 2.0
+	
+	rearWheel += velocity * delta
+	frontWheel += velocity.rotated(Vector3.UP, steerDirection) * delta
+	
+	var newHeading = (frontWheel - rearWheel).normalized()
+	velocity = newHeading * velocity.length()
+	
+	# TODO: This part is question.
+	#rotation = newHeading
+	#rotation_degrees = newHeading
+	#transform.looking_at(Vector3(0, 0, 1), Vector3.UP)
+	rotate(Vector3.UP, steerDirection)
