15564507d77b30c648944c7a098a470eedb24a9d
puzzud <puzzud@gmail.com>
2020-08-17 10:14:56 -0400

    Added win sequence.

--- Project/Scripts/Game.gd
+++ Project/Scripts/Game.gd
@@ -1,11 +1,15 @@
 extends Spatial
 class_name Game
 
+const aiControllerPrefab := preload("res://Scenes/AiController.tscn")
+
 const maxNumberOfLaps := 3
 
 var kartLapNumbers = []
 
 var kartIds = {}
+
+var winnerKartId := -1
 
 func _ready():
 	initializeKartIds()
@@ -14,13 +18,25 @@ func _ready():
 	startTrafficLight()
 
 func _input(event: InputEvent) -> void:
-	if Input.is_key_pressed(KEY_F1):
+	if Input.is_key_pressed(KEY_P):
 		Engine.time_scale = 0.125
 	else:
 		Engine.time_scale = 1.0
+	
+	if Input.is_key_pressed(KEY_F1):
+		restartRace()
 
 func getKartIdFromKart(kart: Kart) -> int:
 	return kartIds[kart]
+
+func getHumanControlledKart() -> Kart:
+	for _kart in $Karts.get_children():
+		var kart: Kart = _kart
+		if kart.has_node("Controller"):
+			if kart.get_node("Controller") is HumanController:
+				return kart
+	
+	return null
 
 func getKartLapNumber(kartId) -> int:
 	return kartLapNumbers[kartId]
@@ -75,13 +91,21 @@ func onTrackKartCrossedFinishLine(kart: Kart) -> void:
 	if kartLapNumber > maxNumberOfLaps + 1:
 		# Race is already over.
 		return
-	elif haveAllKartsFinished():
-		endRace()
+	elif kartLapNumber > maxNumberOfLaps:
+		if winnerKartId == -1:
+			winnerKartId = kartId
+		
+		if kartId == getKartIdFromKart(getHumanControlledKart()):
+			endRace()
 	
 	if haveAllKartsStarted():
 		getTrack().showLapNumber(getHighestLapNumber())
 
 func endRace() -> void:
+	issueRaceResultMessage()
+	automateAllHumanControlledKarts()
+
+func restartRace():
 	get_tree().reload_current_scene()
 
 func startTrafficLight() -> void:
@@ -94,3 +118,25 @@ func unlockAllKarts() -> void:
 	for _kart in $Karts.get_children():
 		var kart: Kart = _kart
 		kart.positionLocked = false
+
+func automateAllHumanControlledKarts() -> void:
+	var kart := getHumanControlledKart()
+	if kart != null:
+		automateKart(kart)
+
+func automateKart(kart: Kart) -> void:
+	kart.remove_child(kart.get_node("Controller"))
+	var aiController := aiControllerPrefab.instance()
+	aiController.name = "Controller"
+	kart.add_child(aiController)
+
+func issueRaceResultMessage() -> void:
+	var kart := getHumanControlledKart()
+	if kart == null:
+		return
+	
+	var kartId := getKartIdFromKart(kart)
+	if kartId == winnerKartId:
+		$Ui/RaceResultMessage.text = "You Have Become a Winner!"
+	else:
+		$Ui/RaceResultMessage.text = "Try Again"
