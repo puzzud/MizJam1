4d62b1dce66387ce05449f3c927a0a43d5bf6df9
puzzud <puzzud@gmail.com>
2020-08-30 17:10:32 -0400

    Significant improvement to kart AI obstacle avoidance.

--- Project/Scripts/AiController.gd
+++ Project/Scripts/AiController.gd
@@ -270,30 +270,12 @@ func areFrontRayCastsCollding() -> bool:
 	
 	return false
 
-func getRayCastDistanceToItsCollider(rayCast: RayCast) -> float:
+func getRayCastScaledDistanceToItsCollider(rayCast: RayCast) -> float:
 	if not rayCast.is_colliding():
-		var distance: float = get_parent().to_global(rayCast.cast_to).length()
-		return distance
+		return 1.0
 	
 	var collider: Spatial = rayCast.get_collider()
-	return rayCast.global_transform.origin.distance_to(collider.global_transform.origin)
-
-func getCloserCollisionRayCast(rayCastA: RayCast, rayCastB: RayCast) -> RayCast:
-	if getRayCastDistanceToItsCollider(rayCastA) < getRayCastDistanceToItsCollider(rayCastB):
-		return rayCastA
-	elif getRayCastDistanceToItsCollider(rayCastA) > getRayCastDistanceToItsCollider(rayCastB):
-		return rayCastB
-	else:
-		return null
-
-func getTurnDirectionFromCloserRayCast(leftRayCast: RayCast, rightRayCast: RayCast) -> float:
-	match getCloserCollisionRayCast(leftRayCast, rightRayCast):
-		leftRayCast:
-			return 1.0
-		rightRayCast:
-			return -1.0
-	
-	return 0.0
+	return Vector3.ZERO.distance_to(rayCast.to_local(rayCast.get_collision_point())) / 10.0
 
 func getTurnDirectionFromRayCasts() -> float:
 	var parent = get_parent()
@@ -309,25 +291,21 @@ func getTurnDirectionFromRayCasts() -> float:
 	var frontRightRayCastColliding := frontRightRayCast.is_colliding()
 	var right15RayCastColliding := right15RayCast.is_colliding()
 	
-	var left15RayCastDistance  := getRayCastDistanceToItsCollider(left15RayCast)
-	var frontLeftRayCastDistance := getRayCastDistanceToItsCollider(frontLeftRayCast)
-	var frontRightRayCastDistance := getRayCastDistanceToItsCollider(frontRightRayCast)
-	var right15RayCastDistance := getRayCastDistanceToItsCollider(right15RayCast)
+	var left15RayCastDistance  := getRayCastScaledDistanceToItsCollider(left15RayCast)
+	var frontLeftRayCastDistance := getRayCastScaledDistanceToItsCollider(frontLeftRayCast)
+	var frontRightRayCastDistance := getRayCastScaledDistanceToItsCollider(frontRightRayCast)
+	var right15RayCastDistance := getRayCastScaledDistanceToItsCollider(right15RayCast)
 	
 	var turnDirection := 0.0
-	turnDirection += 0.125 * (left15RayCastDistance) * left15RayCastColliding as int
-	turnDirection += (frontLeftRayCastDistance) * frontLeftRayCastColliding as int
-	turnDirection += -(frontRightRayCastDistance) * frontRightRayCastColliding as int
-	turnDirection += 0.125 * -(right15RayCastDistance) * right15RayCastColliding as int
-	
-	if turnDirection == 0.0:
-		return 0.0
-	
-	if abs(turnDirection) < 0.125:
-		turnDirection = 0.0
+	turnDirection += 0.62 * -(left15RayCastDistance)
+	turnDirection += -(frontLeftRayCastDistance)
+	turnDirection += (frontRightRayCastDistance)
+	turnDirection += 0.62 * (right15RayCastDistance)
 	
 	if turnDirection != 0.0:
 		turnDirection /= abs(turnDirection)
+	if turnDirection == 0.0:
+		return -1.0
 	
 	return turnDirection
 
